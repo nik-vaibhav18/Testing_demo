1964636969786c81e2e208624963041c9ccc8a30
nik-vaibhav18 <nik.vaibhav18@gmail.com>
2021-09-29 14:52:33 +0530

    added log file

--- and.py
+++ and.py
@@ -6,13 +6,14 @@ import os
 import numpy as np
 
 logging_str="[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
-logging_dir="logs"
+logging_dir= "logs"
 os.makedirs(logging_dir,exist_ok=True)
 logging.basicConfig(filename=os.path.join(logging_dir,"running_logs.log"),level=logging.INFO,format=logging_str)
 
 AND = {"x1": [0,0,1,1],
         "x2": [0,1,0,1],
         "y": [0,0,0,1] }
+
 
 df = pd.DataFrame(AND)
 logging.info(df)
